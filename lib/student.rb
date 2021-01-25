a5a13375676af24e063848cc60fc0a8adb40feb2
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2021-01-25 13:05:45 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -13,15 +13,22 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    # binding.pry
+    students_array.each {|student| student = self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    # binding.pry
+     @twitter = attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin]
+    @github = attributes_hash[:github]
+    @blog = attributes_hash[:blog]
+    @profile_quote = attributes_hash[:profile_quote]
+    @bio = attributes_hash[:bio]
   end
 
   def self.all
-    
+     @@all
   end
 end
 
